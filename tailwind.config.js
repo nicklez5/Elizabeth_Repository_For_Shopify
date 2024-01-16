cbf18a48837a022a8370ffd8f26fd6ddff265e9a
Jackson Lu <jackson2k@yahoo.com>
2024-01-16 04:10:43 -0800

    d

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,11 @@
 module.exports = {
+  content: [
+    './layout/*.liquid',
+    '.sections/*.liquid',
+    './snippets/*.liquid',
+    './templates/customer/*.liquid',
+    './templates/*.liquid'
+  ],
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
